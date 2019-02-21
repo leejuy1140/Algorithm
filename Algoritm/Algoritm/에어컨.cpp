238b71776cc6c3646026508c08c59715b695c533
이주영 <leejuy1140@gmail.com>
2019-02-21 22:17:39 +0900

    190221

--- Algoritm/Algoritm/에어컨.cpp
+++ Algoritm/Algoritm/에어컨.cpp
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <cmath>
+#include <cstdint>
 #include <algorithm>
-#include <climits>
 using namespace std;
 
 const int N_MAX = 1001;
 const int M_MAX = 101;
 
-int dist[N_MAX]; // 사람 위치
-int pos[M_MAX][N_MAX]; // 마지막 에어컨 위치
-
-/* dp[i][j]: 에어컨이 i대일 때,
-			 사람 1 ~ j 까지의 최소 불쾌 지수 */
-long long dp[M_MAX][N_MAX];
+int dist[N_MAX];
+uint64_t sum[N_MAX][N_MAX];
+uint64_t cache[N_MAX][N_MAX];
+uint64_t dp[M_MAX][N_MAX];
 
 int main()
 {
@@ -20,67 +18,49 @@ int main()
 	scanf("%d %d", &n, &m);
 	for (int i = 1; i <= n; i++)
 		scanf("%d", &dist[i]);
-	fill(&dp[1][1], &dp[m + 1][n + 1], LLONG_MAX);
 	sort(&dist[0], &dist[n + 1]);
 
-	/* 에어컨이 1대일 때, 불쾌지수 구하기. */
-	dp[1][1] = 0;
-	pos[1][1] = dist[1];
+	/* sum[i][j]: 1 ~ j까지 에어컨이 i 위치에 있을 때, 불쾌 지수. */
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			sum[i][j] = sum[i][j - 1] + abs(dist[j] - dist[i]);
 
-	for (int j = 2; j <= n; j++)
+	/* cache[i][j]: i ~ j 까지 에어컨이 1대일 때, 최소 불쾌지수. */
+	for (int i = 1; i <= n; i++)
 	{
-		for (int p = dist[j]; p >= pos[1][j - 1]; p--)
+		for (int j = i + 1; j <= n; j++)
 		{
-			long long curSum = 0;
-			for (int k = 1; k <= j; k++)
-				curSum += abs(dist[k] - p);
-
-			if (dp[1][j] <= curSum) break;
-
-			dp[1][j] = curSum;
-			pos[1][j] = p;
+			uint64_t minSum = UINT64_MAX;
+			for (int k = i; k <= n; k++)
+			{
+				uint64_t curSum = sum[k][j] - sum[k][i - 1];
+				if (curSum < minSum) minSum = curSum;
+			}
+			cache[i][j] = minSum;
+			if (i == 1) dp[i][j] = minSum;
 		}
 	}
 
-	/* 2대 이상일 때, 불쾌지수 구하기. */
+	/* dp[i][j]: 에어컨이 i대일 때, 1 ~ j 까지의 최소 불쾌 지수 */
 	for (int i = 2; i <= m; i++)
 	{
-		bool flag = 0;
 		for (int j = i; j <= n; j++)
 		{
-			if (j == i) // 사람 수와 에어컨 수가 같을 때.
-			{
-				dp[i][j] = 0;
-				pos[i][j] = j;
-			}
+			// 사람 수와 에어컨 수가 같을 때.
+			if (j == i) dp[i][i] = 0;
 			else
 			{
-				long long minUncomfort = LLONG_MAX;
-				for (int k = j - 1; k >= i; k--) // k 까지는 이전 그룹
+				uint64_t minSum = dp[i - 1][j - 1];
+				for (int k = j - 2; k >= i; k--) // k 까지는 이전 그룹
 				{
-					long long prevUncomfort = dp[i - 1][k];
-					int prevPos = pos[i - 1][k];
-					if (prevUncomfort >= minUncomfort) break;
-
-					for (int p = prevPos + 1; p <= dist[j]; p++)
-					{
-						long long curUncomfort = 0;
-						for (int sIdx = k + 1; sIdx <= j; sIdx++)
-							curUncomfort += abs(p - dist[sIdx]);
-
-						if (curUncomfort + prevUncomfort >= minUncomfort) break;
-
-						minUncomfort = curUncomfort + prevUncomfort;
-						pos[i][j] = p;
-					}
+					uint64_t curSum = dp[i - 1][k] + cache[k + 1][j];
+					if (curSum < minSum) minSum = curSum;
 				}
-				dp[i][j] = minUncomfort;
+				dp[i][j] = minSum;
 			}
 		}
 	}
-
-	if (dp[m][n] == LLONG_MAX) dp[m][n] = 0;
-	printf("%lld\n", dp[m][n]);
+	printf("%llu\n", dp[m][n]);
 
 	return 0;
 }
