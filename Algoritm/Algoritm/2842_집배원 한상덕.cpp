1b38f8571e129eedd04c6e83adb8450b758438ef
이주영 <leejuy1140@gmail.com>
2019-03-12 21:59:32 +0900

    190312

--- Algoritm/Algoritm/2842_집배원 한상덕.cpp
+++ Algoritm/Algoritm/2842_집배원 한상덕.cpp
@@ -1,95 +1,45 @@
 #include <iostream>
-#include <queue>
+#include <cstring>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 const int MAX = 55;
 const int INF = 99999999;
 
 char map[MAX][MAX];
-int dist[MAX][MAX][2]; // 최저, 최고 높이 저장.
-int n, houseCnt, height[MAX][MAX];
+bool visited[MAX][MAX];
+int n, altitude[MAX][MAX];
 int dr[8] = { -1, 1, 0, 0, -1, -1, 1, 1 };
 int dc[8] = { 0, 0, -1, 1, 1, -1, 1, -1 };
 
-struct PostOfficer { int r, c, highest, lowest, fatigue; };
-
-bool operator>(const PostOfficer &a, const PostOfficer &b) {
-	return a.fatigue > b.fatigue;
-}
-
-inline int max(int a, int b) {
-	return a > b ? a : b;
-}
-
-inline int min(int a, int b) {
-	return a < b ? a : b;
-}
-
-void bfs(int _r, int _c)
+/* min_h와 max_h 범위 내로 배달가능한 집 수 반환. */
+int dfs(int r, int c, int min_h, int max_h)
 {
-	priority_queue<PostOfficer, vector<PostOfficer>, greater<PostOfficer>> pq;
-	pq.push({ _r, _c, height[_r][_c], height[_r][_c], 0 });
-	dist[_r][_c][0] = height[_r][_c];
-	dist[_r][_c][1] = height[_r][_c];
+	if (r < 0 || c < 0 || r >= n || c >= n) return 0;
+	if (visited[r][c]) return 0;
+	if (altitude[r][c] < min_h) return 0;
+	if (altitude[r][c] > max_h) return 0;
 
-	while (!pq.empty())
+	visited[r][c] = 1;
+
+	int ret = 0;
+	if (map[r][c] == 'K') ret++;
+
+	for (int i = 0; i < 8; i++)
 	{
-		PostOfficer cur = pq.top();
-		pq.pop();
-		//printf("(%d, %d) %d %d -> %d\n", cur.r, cur.c, cur.highest, cur.lowest, cur.fatigue);
-
-		for (int i = 0; i < 8; i++)
-		{
-			PostOfficer next = cur;
-			next.r += dr[i];
-			next.c += dc[i];
-			if (next.r < 0 || next.c < 0 || next.r >= n || next.c >= n) continue;
-			if (map[next.r][next.c] == 'P') continue;
-
-			next.lowest = min(next.lowest, height[next.r][next.c]);
-			next.highest = max(next.highest, height[next.r][next.c]);
-			next.fatigue = next.highest - next.lowest;
-
-			if (dist[next.r][next.c][0] == next.lowest)
-				if(dist[next.r][next.c][1] == next.highest) continue;
-			
-			if (dist[next.r][next.c][0] && dist[next.r][next.c][1])
-			{
-				int fatigue = dist[next.r][next.c][1] - dist[next.r][next.c][0];
-				if (fatigue < next.fatigue) continue;
-				if (fatigue == next.fatigue)
-				{
-					if (next.lowest < dist[next.r][next.c][0])
-						if (next.highest > dist[next.r][next.c][1])
-							continue;
-				}
-			}
-
-			pq.push(next);
-			dist[next.r][next.c][0] = next.lowest;
-			dist[next.r][next.c][1] = next.highest;
-		}
+		int nr = r + dr[i];
+		int nc = c + dc[i];
+		ret += dfs(nr, nc, min_h, max_h);
 	}
-	
-	printf("\n");
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-			printf("%d ", dist[i][j][0]);
-		printf("\n");
-	}
-	printf("\n");
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-			printf("%d ", dist[i][j][1]);
-		printf("\n");
-	}
+	return ret;
 }
 
 int main()
 {
-	int post_r, post_c;
+	vector<int> height;
+	int post_r, post_c, houseCnt = 0;
+
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++)
 	{
@@ -106,24 +56,35 @@ int main()
 		}
 	}
 	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			scanf("%d", &height[i][j]);
-
-	bfs(post_r, post_c);
-
-	int highest = 0, lowest = INF;
-	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			if (map[i][j] == 'K')
-			{
-				lowest = min(lowest, dist[i][j][0]);
-				highest = max(highest, dist[i][j][1]);
-			}
+			scanf("%d", &altitude[i][j]);
+			height.push_back(altitude[i][j]);
 		}
 	}
-	printf("%d", highest - lowest);
+
+	/* 1. 높이 정렬 및 중복 제거. */
+	sort(height.begin(), height.end());
+	height.erase(unique(height.begin(), height.end()), height.end());
+
+	/* 2. height[i] ~ height[j]의 범위로 우편물 배달.
+	i와 j는 0부터 시작하기 때문에,
+	- 배달 가능: 최고도인 j가 충분하다는 뜻이므로, i++;
+	- 배달 불가능: 최고도인 j로 부족하다는 뜻이므로, j++;	*/
+	int i = 0, j = i, answer = INF;
+	while (i < height.size() && j < height.size())
+	{
+		memset(visited, 0, sizeof(visited));
+		int finishCnt = dfs(post_r, post_c, height[i], height[j]);
+		if (finishCnt == houseCnt)
+		{
+			answer = min(answer, height[j] - height[i]);
+			i++;
+		}
+		else j++;
+	}
+	printf("%d\n", answer);
 
 	return 0;
 }
