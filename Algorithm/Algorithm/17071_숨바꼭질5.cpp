8336089723ca7a99107532c5ce095344556b7e64
이주영 <leejuy1140@gmail.com>
2019-03-31 17:32:57 +0900

    190331

--- Algorithm/Algorithm/17071_숨바꼭질5.cpp
+++ Algorithm/Algorithm/17071_숨바꼭질5.cpp
@@ -1,65 +1,78 @@
 #include <iostream>
+#include <cstring>
 #include <queue>
 using namespace std;
 
 const int MAX = 500001;
 
-int cony, brown;
-int visited[MAX];
-struct Info { int c, b, c_move, time; };
+/* 수빈이가 특정 위치에 처음 도달한 시간을 짝수, 홀수를 구분해서 저장.
+   수빈이는 특정 칸에 도달하면, +1 -> -1을 반복하며 동생을 기다릴 수 있음.
+   예를 들어, 수빈이가 특정 칸에 짝수 시간에 미리 도달했고,
+   동생도 짝수 시간에 해당 칸에 도착할 예정이라면,
+   수빈이는 +1 -> -1을 반복하며, 해당 칸에서 동생을 만날 수 있음. */
+int subin[2][MAX]; // 0: 짝수 시간, 1: 홀수 시간
 
-int solution() {
-	queue<Info> q;
-	q.push({ cony, brown, 0, 0 });
+/* 수빈이가 특정 칸에 처음으로 도달하는 시간을 짝홀을 구분하여 확인. */
+void CheckSubin(int start)
+{
+	memset(subin, -1, sizeof(subin));
+
+	queue<pair<int, int>> q; // 시간, 위치.
+	q.push(make_pair(0, start));
+	subin[0][start] = 0;
 
 	while (!q.empty())
 	{
-		Info cur = q.front();
+		int curTime = q.front().first;
+		int curPos = q.front().second;
 		q.pop();
-		//printf("%d %d %d\n", cur.c, cur.b, cur.time);
 
-		if (cur.c == cur.b) return cur.time;
-
-		int next_c = cur.c + cur.c_move + 1;
-		if (next_c >= MAX) continue;
-
-		int next_b1 = cur.b + 1;
-		if (next_b1 < MAX && next_b1 >= 0)
+		int next1 = curPos + 1;
+		if (next1 < MAX && next1 >= 0)
 		{
-			if (!cur.time || visited[next_b1] != cur.time)
+			if (subin[(curTime + 1) % 2][next1] == -1)
 			{
-				q.push({ next_c, next_b1, cur.c_move + 1, cur.time + 1 });
-				visited[next_b1] = cur.time;
+				q.push(make_pair(curTime + 1, next1));
+				subin[(curTime + 1) % 2][next1] = curTime + 1;
 			}
 		}
 
-		int next_b2 = cur.b - 1;
-		if (next_b2 < MAX && next_b2 >= 0)
+		int next2 = curPos - 1;
+		if (next2 < MAX && next2 >= 0)
 		{
-			if (!cur.time || visited[next_b2] != cur.time)
+			if (subin[(curTime + 1) % 2][next2] == -1)
 			{
-				q.push({ next_c, next_b2, cur.c_move + 1, cur.time + 1 });
-				visited[next_b2] = cur.time;
+				q.push(make_pair(curTime + 1, next2));
+				subin[(curTime + 1) % 2][next2] = curTime + 1;
 			}
 		}
 
-		int next_b3 = cur.b * 2;
-		if (next_b3 < MAX && next_b3 >= 0)
+		int next3 = curPos * 2;
+		if (next3 < MAX && next3 >= 0)
 		{
-			if (!cur.time || visited[next_b3] != cur.time)
+			if (subin[(curTime + 1) % 2][next3] == -1)
 			{
-				q.push({ next_c, next_b3, cur.c_move + 1, cur.time + 1 });
-				visited[next_b3] = cur.time;
+				q.push(make_pair(curTime + 1, next3));
+				subin[(curTime + 1) % 2][next3] = curTime + 1;
 			}
 		}
 	}
-	return -1;
+}
+
+/* 동생을 시간에 따라 위치시키며, 
+   수빈이가 먼저 도착했었다면, 현재 동생의 시간이 정답. */
+int CheckSister(int cur, int move, int time)
+{
+	if (cur >= MAX) return -1; // 동생이 범위를 벗어나면, 종료.
+	if (subin[time % 2][cur] != -1 && subin[time % 2][cur] <= time) return time;
+	return CheckSister(cur + move, move + 1, time + 1);
 }
 
 int main(void)
 {
-	scanf("%d %d", &brown, &cony);
-	int answer = solution();
-	printf("%d\n", answer);
+	int n, k;
+	cin >> n >> k;
+	CheckSubin(n);				  // 수빈 먼저 확인.
+	cout << CheckSister(k, 1, 0); // 동생 확인.
 	return 0;
 }
